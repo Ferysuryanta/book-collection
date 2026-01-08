08c42163e872c58077ed729360e615badb5e9179
xx-freedom-xx <107397075+Ferysuryanta@users.noreply.github.com>
2026-01-08 16:00:08 +0700

    feat: init Spring Boot project

---
+++ book-collection/src/main/java/com/book/collection/BookCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.book.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCollectionApplication.class, args);
+	}
+
+}
---
+++ book-collection/src/test/java/com/book/collection/BookCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
